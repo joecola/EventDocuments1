fe8e3d9cd5aaa16e92618ed09ada8f6900f0b8ec
Joe Hahn <joehahn@gmail.com>
2017-05-31 14:30:06 -0500

    v1.0.7

--- www/js/index.js
+++ www/js/index.js
@@ -32,3 +32,53 @@ document.getElementById("controls").addEventListener("change", function() {
 document.getElementById("views").addEventListener("click", function(evt) {
   updateView(evt.target.value);
 });
+
+
+$( document ).ready(function() {
+
+
+var pageWidth, pageHeight;
+
+var basePage = {
+  width: 800,
+  height: 600,
+  scale: 1,
+  scaleX: 1,
+  scaleY: 1
+};
+
+$(function(){
+  var $page = $('.page_content');
+
+  getPageSize();
+  scalePages($page, pageWidth, pageHeight);
+
+  //using underscore to delay resize method till finished resizing window
+  $(window).resize(_.debounce(function () {
+    getPageSize();
+    scalePages($page, pageWidth, pageHeight);
+  }, 150));
+
+
+function getPageSize() {
+  pageHeight = $('.section_').height();
+  pageWidth = $('.section_').width();
+}
+
+function scalePages(page, maxWidth, maxHeight) {
+  var scaleX = 1, scaleY = 1;
+  scaleX = maxWidth / basePage.width;
+  scaleY = maxHeight / basePage.height;
+  basePage.scaleX = scaleX;
+  basePage.scaleY = scaleY;
+  basePage.scale = (scaleX > scaleY) ? scaleY : scaleX;
+
+  var newLeftPos = Math.abs(Math.floor(((basePage.width * basePage.scale) - maxWidth)/2));
+  var newTopPos = Math.abs(Math.floor(((basePage.height * basePage.scale) - maxHeight)/2));
+
+  page.attr('style', '-webkit-transform:scale(' + basePage.scale + ');left:' + newLeftPos + 'px;top:' + newTopPos + 'px;');
+}
+});
+
+
+});
